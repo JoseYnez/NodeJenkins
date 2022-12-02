a21e2e15b9e34004e774ed7624e837b59eb5ddc2
Jose Yañez <yanezluis264@gmail.com>
2022-12-01 23:32:41 -0600

    a

--- index.js
+++ index.js
@@ -17,6 +17,10 @@ app.get('/ayuda', function (req, res) {
   res.send('AYUDA');
 });
 
+app.get('/game', function (req, res) {
+  res.send('gam');
+});
+
 var server = app.listen(3000, function () {
   var host = server.address().address;
   var port = server.address().port;
